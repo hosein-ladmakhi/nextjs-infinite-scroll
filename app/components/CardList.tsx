c1599842b81e4ef2ce699d1f12bf50721aa58980
hosein ladmakhi <hoseinladmakhinejad@gmail.com>
2023-10-20 06:06:24 -0700

    Card Refactroing

--- app/components/CardList.tsx
+++ app/components/CardList.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { TCharacter } from "../models";
-import Card from "./Card";
+import Image from "next/image";
 
 interface IProps {
   characters: TCharacter[];
@@ -8,7 +8,22 @@ interface IProps {
 
 const CardList: FC<IProps> = ({ characters }) => {
   return React.Children.toArray(
-    characters.map((character) => <Card character={character} />)
+    characters.map((character) => (
+      <div className="flex my-5 items-center">
+        <div className="relative h-28 w-28 mr-4">
+          <Image
+            alt={character.name}
+            style={{ objectFit: "cover" }}
+            fill
+            src={character.image}
+          />
+        </div>
+        <div>
+          <h1 className="font-bold text-gray-500">{character.name}</h1>
+          <p className="text-gray-500">{character.gender}</p>
+        </div>
+      </div>
+    ))
   );
 };
 
